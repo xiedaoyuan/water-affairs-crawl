6d3672903168a4e445a6976dd742405636f3116a
daoyuan <xiedaoyuan0364@outlook.com>
2025-02-24 10:36:34 +0800

    add explain

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
+// 创建 express 应用实例
 const app = express();
+// 设置端口号，优先使用环境变量中的 PORT，如果没有则使用 3000
 const port = process.env.PORT || 3000;
 
 // 中间件配置
@@ -14,6 +16,7 @@ app.use(express.static('public'));
 
 // 引入路由模块
 const apiRoutes = require('./routes/api');
+// 使用 '/api' 路径前缀应用 apiRoutes
 app.use('/api', apiRoutes);
 
 // 启动服务器
